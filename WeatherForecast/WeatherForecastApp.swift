4928f6d01e969d2971e7c4bd0099a4dc0cc0f54f
Jeremy Kim <jedo@alumni.unc.edu>
2025-03-27 22:34:07 -0400

    Initial Commit

---
+++ WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by Jeremy Kim on 3/27/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherForecastTests/WeatherForecastTests.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherForecastTests.swift
+//  WeatherForecastTests
+//
+//  Created by Jeremy Kim on 3/27/25.
+//
+
+import Testing
+
+struct WeatherForecastTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
