1b414addd53abdb6bd661a0389740053645bfcf3
Jeremy Kim <jedo@alumni.unc.edu>
2025-03-29 23:38:20 -0400

    day update

--- WeatherForecast/ContentView.swift
+++ WeatherForecast/ContentView.swift
@@ -11,18 +11,11 @@ struct ContentView: View {
 
     var body: some View {
 
-        VStack {
-
-            Text("Mon")
-            Image(systemName: "sun.max.fill")
-                .foregroundStyle(Color.yellow)
-            Text("High: 70")
-
-            Text("Low: 50")
-
+        HStack {
+            DayForecast(day: "Mon")
+            
+            DayForecast(day: "Tue")
         }
-
-        .padding()
 
     }
 
@@ -30,4 +23,23 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+struct DayForecast: View {
+    let day: String
+    
+    var body: some View {
+        VStack {
+            
+            Text(day)
+            Image(systemName: "sun.max.fill")
+                .foregroundStyle(Color.yellow)
+            Text("High: 70")
+            
+            Text("Low: 50")
+            
+        }
+        
+        .padding()
+    }
 }
