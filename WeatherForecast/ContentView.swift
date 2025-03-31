66fb2df5252a05a5e201128cfc708056f46d9f4b
Jeremy Kim <jedo@alumni.unc.edu>
2025-03-31 14:56:24 -0400

    weather app update

--- WeatherForecast/ContentView.swift
+++ WeatherForecast/ContentView.swift
@@ -12,9 +12,9 @@ struct ContentView: View {
     var body: some View {
 
         HStack {
-            DayForecast(day: "Mon", high: 70, low: 50)
+            DayForecast(day: "Mon", high: 70, low: 50, isRainy: false)
             
-            DayForecast(day: "Tue", high: 60, low:40)
+            DayForecast(day: "Tue", high: 60, low:40, isRainy: true)
         }
 
     }
@@ -29,17 +29,39 @@ struct DayForecast: View {
     let day: String
     let high: Int
     let low: Int
+    let isRainy: Bool
+    
+    var iconName: String {
+        if isRainy {
+            return "cloud.rain.fill"
+        } else {
+            return "sun.max.fill"
+        }
+        
+    }
+    
+    var iconColor: Color {
+        if isRainy {
+            return Color.blue
+        } else {
+            return Color.yellow
+        }
+    }
     
     var body: some View {
         VStack {
             
             Text(day)
-            Image(systemName: "sun.max.fill")
-                .foregroundStyle(Color.yellow)
-            Text("\(high)")
-            
-            Text("\(low)")
-            
+                .font(Font.headline)
+            Image(systemName: iconName)
+                .foregroundStyle(iconColor)
+                .font(Font.largeTitle)
+                .padding(5)
+            Text("\(high)º")
+                .fontWeight(Font.Weight.semibold)
+            Text("\(low)º")
+                .fontWeight(Font.Weight.medium)
+                .foregroundStyle(Color.secondary)
         }
         
         .padding()
